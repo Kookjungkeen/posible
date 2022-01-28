9f491afa33a4931637aa1c9eb9e17f0b0afb2b83
Kookjungkeeen <cat3668@naver.com>
2022-01-29 02:09:14 +0900

    - TableViewController 생성 후 Navigation Controller추가 -  viewController 생성 세그웨이 연결 - 새로운  swift 파일 만든 후 viewController 마다 연결

---
+++ posible/AddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewController.swift
+//  posible
+//
+//  Created by 국정근 on 2022/01/29.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ posible/TableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TableViewController.swift
+//  posible
+//
+//  Created by 국정근 on 2022/01/29.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
